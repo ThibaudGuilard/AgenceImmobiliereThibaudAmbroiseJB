c6184b6adb51cd845a49b3e0aaa043a9094be547
AmbroiseRENE <ambroise.rene@gmail.com>
2019-11-14 14:47:54 +0100

    ajout annotation client controller

--- AgenceImmobiliereThibaudAmbroiseJB/src/main/java/com/fr/adaming/web/controller/impl/ClientControllerImpl.java
+++ AgenceImmobiliereThibaudAmbroiseJB/src/main/java/com/fr/adaming/web/controller/impl/ClientControllerImpl.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fr.adaming.entity.Client;
@@ -20,6 +21,7 @@ import com.fr.adaming.web.dto.ClientDto;
  *
  */
 @RestController
+@RequestMapping(path = "api/client")
 public class ClientControllerImpl implements IClientController {
 
 	@Autowired
