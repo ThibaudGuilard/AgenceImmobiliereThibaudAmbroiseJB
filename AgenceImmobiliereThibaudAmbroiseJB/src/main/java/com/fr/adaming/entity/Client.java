4df777ef52f7da693690d65b39e109e1e4bf1268
AmbroiseRENE <ambroise.rene@gmail.com>
2019-11-14 12:14:57 +0100

    Mise a jour dse entotes

--- AgenceImmobiliereThibaudAmbroiseJB/src/main/java/com/fr/adaming/entity/Client.java
+++ AgenceImmobiliereThibaudAmbroiseJB/src/main/java/com/fr/adaming/entity/Client.java
@@ -1,9 +1,14 @@
 package com.fr.adaming.entity;
 
+import javax.persistence.Entity;
+
+import com.fr.adaming.entity.enume.TypeClient;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter @Setter @NoArgsConstructor
 public class Client extends User{
 	
