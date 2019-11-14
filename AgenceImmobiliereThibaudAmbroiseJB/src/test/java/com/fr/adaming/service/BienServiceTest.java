8d46d56cffc9069454cda207af84c0618cae1462
AmbroiseRENE <ambroise.rene@gmail.com>
2019-11-14 16:40:31 +0100

    ajout annotation class BienTest

--- AgenceImmobiliereThibaudAmbroiseJB/src/test/java/com/fr/adaming/service/BienServiceTest.java
+++ AgenceImmobiliereThibaudAmbroiseJB/src/test/java/com/fr/adaming/service/BienServiceTest.java
@@ -1,5 +1,11 @@
 package com.fr.adaming.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class BienServiceTest {
 
+	@Autowired
+	private IBienService service;
 }
