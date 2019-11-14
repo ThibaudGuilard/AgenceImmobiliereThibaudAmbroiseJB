7ae65f876c1180e4200111255a59fcb627459bb7
Thibaud Guilard <thibaudguilard@free.fr>
2019-11-14 12:35:43 +0100

    maj Client

--- AgenceImmobiliereThibaudAmbroiseJB/src/main/java/com/fr/adaming/entity/Client.java
+++ AgenceImmobiliereThibaudAmbroiseJB/src/main/java/com/fr/adaming/entity/Client.java
@@ -1,6 +1,11 @@
 package com.fr.adaming.entity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import com.fr.adaming.entity.enume.TypeClient;
 
@@ -13,6 +18,10 @@ import lombok.Setter;
 public class Client extends User{
 	
 	private Enum<TypeClient> type;
+	@ManyToOne @JoinColumn(name = "id_agent")
+	private Agent agent;
+	@OneToMany(mappedBy = "client")
+	private List<Bien> biens;
 
 	public Client(String email, String fullName, int telephone, Enum<TypeClient> type) {
 		super();
