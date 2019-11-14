3ca6d17b49bbf1170b0fc4726f7e948ef9032628
jbjamet <jb.jamet@outlook.fr>
2019-11-14 12:30:03 +0100

    Ajout d'un constructeur à la classe Agent

--- AgenceImmobiliereThibaudAmbroiseJB/src/main/java/com/fr/adaming/entity/Agent.java
+++ AgenceImmobiliereThibaudAmbroiseJB/src/main/java/com/fr/adaming/entity/Agent.java
@@ -3,6 +3,8 @@ package com.fr.adaming.entity;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 
@@ -18,6 +20,15 @@ public class Agent extends User {
 	private String pwd;
 	@PastOrPresent
 	private LocalDate dateRecrutement;
+	
+	public Agent(@Email @NotNull String email, @NotNull String fullName, @Pattern(regexp = "\\d{10}") int telephone,
+			boolean deleted, @Pattern(regexp = "^.{8,16}$") String pwd, @PastOrPresent LocalDate dateRecrutement) {
+		super(email, fullName, telephone, deleted);
+		this.pwd = pwd;
+		this.dateRecrutement = dateRecrutement;
+	}
+	
+	
 	
 
 }
