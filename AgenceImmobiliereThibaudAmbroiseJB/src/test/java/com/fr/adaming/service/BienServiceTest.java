1dfcbcdbca7d5c8f850ffa2ba385c6cd842fa514
Thibaud Guilard <thibaudguilard@free.fr>
2019-11-14 16:59:23 +0100

    debut test

--- AgenceImmobiliereThibaudAmbroiseJB/src/test/java/com/fr/adaming/service/BienServiceTest.java
+++ AgenceImmobiliereThibaudAmbroiseJB/src/test/java/com/fr/adaming/service/BienServiceTest.java
@@ -2,10 +2,30 @@ package com.fr.adaming.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+
+import com.fr.adaming.entity.Bien;
 
 @SpringBootTest
 public class BienServiceTest {
 
 	@Autowired
 	private IBienService service;
+	
+	@Sql(statements = { "truncate Bien","insert into bien values (112, 200000, false, false)" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "delete from bien where id=112", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+
+	
+	public void updateBienServiceExistant_shouldReturnTrue() {
+		//preparer les inputs
+		Bien bien = new Bien();
+		bien.setId(112L);
+		bien.setPrix(200000);
+		bien.setVendu(true);
+		bien.setDeleted(false);
+		//invoquer la méthode
+		
+		//récupérer le résultat
+	}
 }
