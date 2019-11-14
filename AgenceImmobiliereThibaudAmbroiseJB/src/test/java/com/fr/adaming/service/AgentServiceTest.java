27d583d084417316bc24e7dc57c6feeec61269a1
Thibaud Guilard <thibaudguilard@free.fr>
2019-11-14 18:35:13 +0100

    ajout tests updateAgent ok et pas existant

--- AgenceImmobiliereThibaudAmbroiseJB/src/test/java/com/fr/adaming/service/AgentServiceTest.java
+++ AgenceImmobiliereThibaudAmbroiseJB/src/test/java/com/fr/adaming/service/AgentServiceTest.java
@@ -1,5 +1,52 @@
 package com.fr.adaming.service;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+
+import com.fr.adaming.entity.Agent;
+import com.fr.adaming.entity.Bien;
+
+@SpringBootTest
 public class AgentServiceTest {
+	
+	@Autowired
+	private IAgentService service;
+	
+	@Sql(statements = { "truncate Agent","insert into agent values (112, 'agent@mail.com', 'John Doe', 88888888, false, 'azertyui', 10/12/2009)","insert into agent values (112, 'agent@mail.com', 'John Doe', 88888888, false, 'azertyui', 10/12/2009)" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "delete from bien where id=112", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+
+	@Test
+	public void updateAgentServiceExistant_shouldReturnTrue() {
+		//preparer les inputs
+		Agent agent = new Agent();
+		agent.setId(112L);
+		agent.setEmail("agent@mail.fr");
+		agent.setFullName("Jane Doe");
+		agent.setPwd("abcd1234");
+		//invoquer la méthode
+		boolean retour = service.updateAgent(agent);
+		//vérifier le résultat
+		assertTrue(retour);
+	}
+	
+	@Test
+	public void updateBienServicePasEnregistre_shouldReturnFalse() {
+		//preparer les inputs
+		Agent agent = new Agent();
+		agent.setId(1120L);
+		agent.setEmail("agent@mail.fr");
+		agent.setFullName("Jane Doe");
+		agent.setPwd("abcd1234");
+		//invoquer la méthode
+		boolean retour = service.updateAgent(agent);
+		//vérifier le résultat
+		assertFalse(retour);
+	}
 
 }
