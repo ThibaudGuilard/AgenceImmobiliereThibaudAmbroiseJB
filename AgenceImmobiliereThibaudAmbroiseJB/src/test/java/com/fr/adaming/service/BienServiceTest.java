a3743e55df472609f13dd92d2f536207899f24e3
jbjamet <jb.jamet@outlook.fr>
2019-11-15 08:29:25 +0100

    Modifs de Tests et du repository de Bien

--- AgenceImmobiliereThibaudAmbroiseJB/src/test/java/com/fr/adaming/service/BienServiceTest.java
+++ AgenceImmobiliereThibaudAmbroiseJB/src/test/java/com/fr/adaming/service/BienServiceTest.java
@@ -3,7 +3,11 @@ package com.fr.adaming.service;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.NoSuchElementException;
+
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
@@ -18,7 +22,7 @@ public class BienServiceTest {
 	private IBienService service;
 	
 	@Test
-	@Sql(statements = "delete from user where prix = 51234565.55 and vendu = true and deleted = false ", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+	@Sql(statements = "delete from bien where prix = 51234565.55 and vendu = true and deleted = false ", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 	public void createValidBien_shouldReturnUserWithIdNotNull() {
 		//preparer les inputs
 		Bien b= new Bien();
@@ -44,21 +48,22 @@ public class BienServiceTest {
 	
 
 
-	@Test
-//	@Sql(statements = "insert into bien values (1234567,false,15,false,1)")
-	private void deleteBienThatExists_shouldReturnTrue() {
-		Bien bien = service.FindParId(1234567L);
-		assertTrue(service.deleteBien(bien));		
-	}
-	
-//	@Rule
-//	ExpectedException exception = ExpectedException.none();
-//	
 //	@Test
-//	private void deleteBienThatDoesNotExist_shouldReturnFalse() {
-//		Bien bien = service.FindParId(5425698754212L);
-//		exception.expect(type);		
+////	@Sql(statements = "insert into bien values (1234567,false,15,false,1)")
+//	private void deleteBienThatExists_shouldReturnTrue() {
+//		Bien bien = service.FindParId(1234567L);
+//		assertTrue(service.deleteBien(bien));		
 //	}
+	
+	@Rule
+	ExpectedException exception = ExpectedException.none();
+	
+	@Test
+	private void deleteBienThatDoesNotExist_shouldReturnFalse() {
+		exception.expect(NoSuchElementException.class);
+		service.FindParId(5425698754212L);
+				
+	}
 	
 
 	@Sql(statements = { "truncate Bien","insert into bien values (112, 200000, false, false)" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
