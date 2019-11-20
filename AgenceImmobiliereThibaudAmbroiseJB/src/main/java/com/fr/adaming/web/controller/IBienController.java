dcb0fe2164c8f47d25a2d6de4d434df3c450acf9
AmbroiseRENE <ambroise.rene.73@gmail.com>
2019-11-20 16:56:04 +0100

    add swagger

--- AgenceImmobiliereThibaudAmbroiseJB/src/main/java/com/fr/adaming/web/controller/IBienController.java
+++ AgenceImmobiliereThibaudAmbroiseJB/src/main/java/com/fr/adaming/web/controller/IBienController.java
@@ -5,20 +5,33 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.fr.adaming.entity.Bien;
 import com.fr.adaming.web.dto.BienDto;
 
 @CrossOrigin
+@RequestMapping(path = "api/bien")
 public interface IBienController {
 
+	@RequestMapping(path = "/add", method = RequestMethod.POST)
 	public Bien createBien (@RequestBody @Valid BienDto dto);
 	
+	@GetMapping(path = "/{id}/searchbyid")
+	public Bien searchParId(@PathVariable(name = "id") long id);
+	
+	@GetMapping(path = "/print")
 	public List<Bien> findAll();
 	
+	@RequestMapping(path = "/update", method = RequestMethod.POST)
 	public Bien updateBien(@RequestBody @Valid BienDto dto);
 	
-	public Bien deleteBien(long id);
+	@PostMapping(path="{id}/delete")
+	public Bien deleteBien(@PathVariable(name ="id")long id);
 	
 }
